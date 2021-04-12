1e6c7a002590cd95c89e224d9592607eb357d5e2
lorenasg1 <lorenasg1@gmail.com>
2021-04-12 20:03:00 -0300

    refactor: server

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import express, { json } from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import { router } from './routes';
+import swaggerFile from './swagger.json';
+
+import './database';
+
+const app = express();
+app.use(json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
+
+export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,3 @@
-import express, { json } from 'express';
-import swaggerUi from 'swagger-ui-express';
-import swaggerFile from './swagger.json';
-
-import { router } from './routes';
-
-const app = express();
-app.use(json());
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(router);
+import { app } from './app';
 
 app.listen(3333, () => console.log('🚀 Server is running on port 3333!'));
