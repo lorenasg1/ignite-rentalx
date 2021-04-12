4ba4256b9b55698648b7a97489a3715770d0c0b6
lorenasg1 <lorenasg1@gmail.com>
2021-04-11 22:34:40 -0300

    chore: add documentation

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express, { json } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
+
 import { router } from './routes';
 
 const app = express();
 app.use(json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
