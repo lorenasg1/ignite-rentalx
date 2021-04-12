1f359df24b78f4411c5407a72a70ff346a109549
lorenasg1 <lorenasg1@gmail.com>
2021-04-11 22:34:02 -0300

    feat: import categories (file upload)

---
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import csvParse from 'csv-parse';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+export class ImportCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+
+      const parsedFile = csvParse();
+
+      stream.pipe(parsedFile);
+
+      parsedFile
+        .on('data', async line => {
+          const [name, description] = line;
+
+          categories.push({ name, description });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', err => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.map(category => {
+      const { name, description } = category;
+
+      const categoryExists = this.categoriesRepository.findByName(name);
+
+      if (!categoryExists) {
+        this.categoriesRepository.create({ name, description });
+      }
+    });
+  }
+}
