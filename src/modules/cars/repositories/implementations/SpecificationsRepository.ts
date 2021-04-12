17ca63d61ec267f3e97ec8631045a74c872f009a
lorenasg1 <lorenasg1@gmail.com>
2021-04-11 22:33:12 -0300

    fix: findByName return type

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -23,7 +23,7 @@ export class SpecificationsRepository implements ISpecificationsRepository {
     this.specifications.push(specification);
   }
 
-  findByName(name: string) {
+  findByName(name: string): Specification {
     const specification = this.specifications.find(
       specification => specification.name === name,
     );
