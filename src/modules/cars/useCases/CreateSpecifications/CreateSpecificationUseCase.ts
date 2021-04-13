f00a4fa99499fa64265fe477d467fb05c33ba7f3
lorenasg1 <lorenasg1@gmail.com>
2021-04-13 14:04:08 -0300

    feat: add users

--- src/modules/cars/useCases/CreateSpecifications/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/CreateSpecifications/CreateSpecificationUseCase.ts
@@ -1,15 +1,21 @@
+import { inject, injectable } from 'tsyringe';
 import { ISpecificationsRepository } from '../../repositories/ISpecificationsRepository';
 
 interface IRequest {
   name: string;
   description: string;
 }
-
+@injectable()
 export class CreateSpecificationUseCase {
-  constructor(private specificationsRepository: ISpecificationsRepository) {}
+  constructor(
+    @inject('SpecificationsRepository')
+    private specificationsRepository: ISpecificationsRepository,
+  ) {}
 
-  execute({ name, description }: IRequest): void {
-    const specificationExists = this.specificationsRepository.findByName(name);
+  async execute({ name, description }: IRequest): Promise<void> {
+    const specificationExists = await this.specificationsRepository.findByName(
+      name,
+    );
 
     if (specificationExists) {
       throw new Error('Specification already exists');
