c2e3b1d626c917388fe18a8ec4ca263c1698835e
lorenasg1 <lorenasg1@gmail.com>
2021-05-01 17:21:35 -0300

    feat: add method compareInDays

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -1,5 +1,6 @@
 export interface IDateProvider {
   convertToUTC(date: Date): string;
   dateNow(): Date;
-  compareInHours(start_date: Date, end_date: Date): Number;
+  compareInHours(start_date: Date, end_date: Date): number;
+  compareInDays(start_date: Date, end_date: Date): number;
 }
